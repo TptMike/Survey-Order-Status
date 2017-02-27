4dbcd024c8ff2949cf0e73d0eed63e4bd99efa95
tptmike <mike.wildcats@gmail.com>
2017-02-27 17:13:36 -0500

    Add first insertion sequence. Will most likely change to a method for the ACP call.

--- surveyorderstatus/class.csvParser.php
+++ surveyorderstatus/class.csvParser.php
@@ -30,7 +30,21 @@ public function parse_csv( $delimiter )
     }
     
     // TODO: Insert to database.
+    global $wpdb;
     
+    $table_name = $wpdb->prefix . "orderstatussurvey";
+    // TODO: Check if table is empty. Right now assume it isn't.
+    $sql = "TRUNCATE $table_name";
+    $dbDelta ( $sql );
+    
+    // Iterate through the array, insert data.
+    foreach ($array as $arr_index)
+    {
+    	$sql = "INSERT INTO $table_name (workorderid, status)
+    	VALUES ($arr_index['workorderid'], $arr_index['status']);";
+    	// run it
+    	dbDelta( $sql );
+    }   
 }
 
 }
